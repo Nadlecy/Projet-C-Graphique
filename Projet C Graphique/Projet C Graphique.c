947571d45686bd81fe8d9545595e2645726188ff
Nadlecy <bruno.cleymand@gmail.com>
2023-03-03 15:29:21 +0100

    update -> 0.2

--- Projet C Graphique/Projet C Graphique.c
+++ Projet C Graphique/Projet C Graphique.c
@@ -431,42 +431,6 @@ int playAgain() {
 //main operating function, runs the whole program
 int main() {
 
-	fullscreen = false;
-	int flags = 0;
-	flags = SDL_WINDOW_RESIZABLE;
-	if (fullscreen) {
-		flags = flags | SDL_WINDOW_FULLSCREEN;
-	}
-	if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
-		printf("Subsystems Initialized!\n");
-
-		window = SDL_CreateWindow("Test Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, flags);
-		if (window) {
-			printf("Window Created!\n");
-			SDL_SetWindowMinimumSize(window, 100, 100);
-		}
-		renderer = SDL_CreateRenderer(window, -1, 0);
-		if (renderer) {
-			SDL_SetRenderDrawColor(renderer, 121, 121, 121, 255);
-			printf("Renderer created!\n");
-			SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
-			isRunning = true;
-		}
-
-	}
-
-	while (isRunning) {
-		handleEvents();
-		update();
-		render();
-	}
-
-	//frees memory associated with renderer and window
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);	//error here
-	SDL_Quit();
-
-
 	int playing = 1;
 	while (playing)
 	{
@@ -485,6 +449,42 @@ int main() {
 		bombPlacing(tab, &rules);
 		bombRadar(tab, &rules);
 
+		//game window creation
+		fullscreen = false;
+		int flags = 0;
+		flags = SDL_WINDOW_RESIZABLE;
+		if (fullscreen) {
+			flags = flags | SDL_WINDOW_FULLSCREEN;
+		}
+		if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+			printf("Subsystems Initialized!\n");
+
+			window = SDL_CreateWindow("main window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, flags);
+			if (window) {
+				printf("Window Created!\n");
+				SDL_SetWindowMinimumSize(window, xSize * 10 + 100, ySize * 10 + 100);
+			}
+			renderer = SDL_CreateRenderer(window, -1, 0);
+			if (renderer) {
+				SDL_SetRenderDrawColor(renderer, 121, 121, 121, 255);
+				printf("Renderer created!\n");
+				SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+				isRunning = true;
+			}
+
+		}
+		SDL_RenderDrawLine(window, 100, 100, 200, 200);
+		while (isRunning) {
+			handleEvents();
+			update();
+			render();
+		}
+
+		//frees memory associated with renderer and window
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);	//error here
+		SDL_Quit();
+
 		//gameplay loop of interacting with the grid
 		gamePlay(tab, &rules);
 
@@ -499,6 +499,7 @@ int main() {
 		//ask player if they want to play again
 		playing = playAgain();
 	}
+
 	return 0;
 }
 
